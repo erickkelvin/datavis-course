522c8c74f8fac5bada392aebde869b2e0f457434
Erick Kelvin <erickkelvin@hotmail.com>
2017-11-12 15:13:38 -0300

    added function to automatically scroll when clicking if needed

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ $(document).ready(function () {
         if (!$(this).hasClass('active')) {
             $('.activeable').removeClass('active');
             $(this).addClass('active');
+            scrollToView($(this));
         }
         else {
             $('.activeable').removeClass('active');
+            $('html,body').animate({scrollTop: 0}, 300);
         }
         //e.preventDefault();
     });
 });
+
+function scrollToView(element){
+    var offset = element.offset().top - $(window).scrollTop();
+    if(offset > (window.innerHeight - 200)){
+        $('html,body').animate({scrollTop: offset}, 300);
+    }
+}
